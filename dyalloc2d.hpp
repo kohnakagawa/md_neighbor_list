73d6abf16dd70ef042772197f219a8cbc6a61bc7
kohnakagawa <tsunekou1019@gmail.com>
2016-12-17 16:39:33 +0900

    アラインメントを指定する関数を追加

--- dyalloc2d.hpp
+++ dyalloc2d.hpp
@@ -1,5 +1,30 @@
 #pragma once
 
+#include <iostream>
+#include <cstdlib>
+
+template <typename T, int alignment>
+void allocate2D_aligend(const int Nx,
+                        const int Ny,
+                        T**& ptr) {
+  ptr = static_cast<T**>(malloc(Nx * sizeof(T*)));
+  const auto stat = posix_memalign(reinterpret_cast<void**>(ptr),
+                                   alignment,
+                                   sizeof(T) * Nx * Ny);
+  if (stat) {
+    std::cerr << "error occurs at allocate2D_aligend\n";
+    std::exit(1);
+  }
+  for (int i = 1; i < Nx; i++) ptr[i] = ptr[0] + i * Ny;
+}
+
+template <typename T>
+void deallocate2D_aligend(T**& ptr) {
+  free(ptr[0]);
+  free(ptr);
+  ptr = nullptr;
+}
+
 template <typename T>
 void allocate2D(const int Nx,
                 const int Ny,
